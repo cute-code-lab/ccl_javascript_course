d8ee8428929fb5b4fc954f15e8acc517ce6ea3c7
cute-code-lab <cutecodelab@gmail.com>
2025-11-30 12:36:18 +0700

    3-Console Log

---
+++ 3-Console Log/script.js
@@ -0,0 +1,23 @@
+// Test Script to verify connection
+console.log("JS file is connected!");
+
+// Examples of console.log() with different data types
+console.log("Hello World!");
+console.log(42);
+console.log("Alice is", 30, "years old.");
+console.log(true);
+const customerName = "Jane";
+console.log(customerName);
+
+// Debugging Errors
+// console.log(customerAge);
+
+// Best Practices for Using console.log()
+// 1. Use descriptive messages
+const username = "john_doe";
+console.log("Username entered:", username);
+// 2. Log variable values at different points in your code
+let count = 0;
+console.log("Before:", count);
+count++;
+console.log("After:", count);
