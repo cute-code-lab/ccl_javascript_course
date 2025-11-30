73d52fda5b43adaf471df41096021f2fb2e153dc
cute-code-lab <cutecodelab@gmail.com>
2025-11-30 13:49:46 +0700

    4-Comments

---
+++ 4-Comments/script.js
@@ -0,0 +1,30 @@
+// SINGLE LINE COMMENT
+// This prints a message to the console
+console.log("Hello from the comments script!");
+
+// This variable stores a student's age
+let age = 20;
+
+let city = "Atlanta"; // This variable stores the city name
+
+// MULTI-LINE COMMENT
+/*
+This function will print something to the console.
+*/
+
+function print() {
+  console.log("Print something");
+}
+
+// Preventing code execution
+
+// console.log("This will not run.");
+console.log("This will run.");
+
+/*function printName() {
+ 
+    console.log("John Doe");
+}
+
+console.log(sum());
+*/
